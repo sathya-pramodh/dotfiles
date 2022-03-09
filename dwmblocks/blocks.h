4d83ffbe52652b8a6c3d9457bd8fb93f4ce0f35b
sathya-pramodh <sathyapramodh17@gmail.com>
2022-03-09 13:24:09 +0530

    Patch correction and dwmblocks config

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,10 +3,10 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" ", "date '+%I:%M%p'",					5,		0},
 	{" RAM: ", "/home/pramodhsathya/dwm/dotfiles/dwmblocks/scripts/freemem",	1,		0},
-	{" ", "uname -r | sed s/-arch1-1//g",0,0},
+	{" ", "uname -r | sed s/-arch1-1//g", 0, 0},
 	{"","/home/pramodhsathya/dwm/dotfiles/dwmblocks/scripts/battery", 1, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " ";
+static char delim[] = "   ";
 static unsigned int delimLen = 5;
